391b3083bb368a0f6d5526b16ae586bd444d2058
audrummer15 <audrummer15@gmail.com>
2020-03-23 00:49:24 -0400

    * Update banner

--- Changeling/Changeling.cs
+++ Changeling/Changeling.cs
@@ -1,5 +1,8 @@
-﻿using CommandLine;
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
 using Changeling.Modules;
+using System.Collections.Generic;
 
 namespace Changeling
 {
@@ -7,12 +10,39 @@ namespace Changeling
     {
         static int Main(string[] args)
         {
-            return Parser.Default.ParseArguments<ExtractCommandOptions, ListCommandOptions, ReplaceCommandOptions>(args)
-                .MapResult(
-                  (ExtractCommandOptions opts) => CommandCollection.ExtractCommand(opts),
-                  (ListCommandOptions opts) => CommandCollection.ListCommand(opts),
-                  (ReplaceCommandOptions opts) => CommandCollection.ReplaceCommand(opts),
-                  errs => 1);
+            var parserResult = Parser.Default.ParseArguments<ExtractCommandOptions, ListCommandOptions, ReplaceCommandOptions>(args);
+            return parserResult.MapResult(
+                (ExtractCommandOptions opts) => CommandCollection.ExtractCommand(opts),
+                (ListCommandOptions opts) => CommandCollection.ListCommand(opts),
+                (ReplaceCommandOptions opts) => CommandCollection.ReplaceCommand(opts),
+                errs => DisplayHelp(parserResult, errs));
+        }
+
+        static int DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.Heading = @"
+..................................-+*#@@#=*+-..................................
+.*@@@#=*:-...................:=WWW@*:-...-+=@WWW=:.............................
+@@-...-+=#WWWWW@=+-......-=WWW*................-+@WW+..........................
+*W#....+*:.....-+=@WWWWWWW#:.......................-=W@#######@@@@@@WWWWWWWWWWW
+..=W=......+#@*:................:@*...:*...-=..........:----..................#
+....#W*........:=WW#:..............=+..@...=-.................:**=====*:....-@W
+.....:W#...........=WW:...:==*=@#:..+-.......+@#*==+......*WW*-............#W=.
+......-@#...........-@W::=+@WWWW@+=.........-#WWWWW@=:..+W@-.............+W@-..
+.......-@#...........+@-:WW@#WWWWW#........+W@#WWWWWW#:-W*.............:@W:....
+.........#W+..........#.-WWWWWWWWWW+.*+:*=-WWWWWWWWWW+..=-...........-@W=......
+..........:@W=-.......*-..+@WWWWWW#-.+=:=-.+WWWWWW@+....*..........+@W=........
+.............+@W@*-....+..............................-#.....-+#WWW#:..........
+................-+=@WWW@W+.......-..::---:*..---.-:*#WW@WWWWW@=:...............
+                "; //  https://www.topster.net/ascii-generator/
+
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e, true);
+
+            Console.WriteLine(helpText);
+            return 1;
         }
     }
 }
