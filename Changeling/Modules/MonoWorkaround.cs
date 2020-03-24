4e35705f0114061c56f27160a37785ace3d26179
Adam <1392689+coffeegist@users.noreply.github.com>
2020-03-24 00:13:23 -0400

    * Add space to list output

---
+++ Changeling/Modules/MonoWorkaround.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Changeling.Modules
+{
+    static class MonoWorkaround
+    {
+        public static void WorkAroundMe()
+        {
+            Console.WriteLine("Loading...");
+        }
+    }
+}
